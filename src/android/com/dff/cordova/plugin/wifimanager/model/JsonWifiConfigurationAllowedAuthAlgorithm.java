225789c17c6cc5ac61b3707a61bc36d313479b25
Frank Koenigstein <frank.koenigstein@dff-solutions.de>
2016-03-23 14:46:58 +0100

    enhanced wificonfiguration json parsing

---
+++ src/android/com/dff/cordova/plugin/wifimanager/model/JsonWifiConfigurationAllowedAuthAlgorithm.java
@@ -0,0 +1,41 @@
+package com.dff.cordova.plugin.wifimanager.model;
+
+import java.util.BitSet;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import com.dff.cordova.plugin.common.log.CordovaPluginLog;
+
+import android.net.wifi.WifiConfiguration;
+
+public class JsonWifiConfigurationAllowedAuthAlgorithm {
+	protected BitSet allowedAuthAlgorithms;
+	
+	private JsonWifiConfigurationAllowedAuthAlgorithm(BitSet allowedAuthAlgorithms) {
+		this.allowedAuthAlgorithms = allowedAuthAlgorithms;
+	}
+	
+	public JSONArray toJson() throws JSONException {
+		JSONArray jsonAllowedAuthAlgorithms = new JSONArray();
+		
+		if (this.allowedAuthAlgorithms != null) {			
+			if (allowedAuthAlgorithms.get(WifiConfiguration.AuthAlgorithm.LEAP)) {
+				jsonAllowedAuthAlgorithms.put("LEAP");
+			}
+			if (allowedAuthAlgorithms.get(WifiConfiguration.AuthAlgorithm.OPEN)) {
+				jsonAllowedAuthAlgorithms.put("OPEN");
+			}
+			
+			if (allowedAuthAlgorithms.get(WifiConfiguration.AuthAlgorithm.SHARED)) {
+				jsonAllowedAuthAlgorithms.put("SHARED");
+			}
+		}
+		
+		return jsonAllowedAuthAlgorithms;
+	}
+	
+	public static JsonWifiConfigurationAllowedAuthAlgorithm getInstance(BitSet allowedAuthAlgorithms) {
+		return new JsonWifiConfigurationAllowedAuthAlgorithm(allowedAuthAlgorithms);
+	}
+}
